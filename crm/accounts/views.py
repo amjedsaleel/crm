cc7bfa69ab7b2f739942f5c33fe8791764bfebc7
Amjed Saleel K <amjedsaleel@gmail.com>
2020-06-23 14:10:53 +0530

    login authentication applied

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from .models import *
@@ -15,11 +17,23 @@ def redisterPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Registration was completed for ' + user)
+            return redirect('login')
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 
 def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
     context = {}
     return render(request, 'accounts/login.html')
 
