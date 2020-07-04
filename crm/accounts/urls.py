ffcd78586dfdb0e6c5bfc8dd7f1f0c9634121178
Amjed Saleel K <amjedsaleel@gmail.com>
2020-07-04 15:55:36 +0530

    password reset setuped ,inbuild

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
 
+from . import views
 
 urlpatterns = [
     path('register/', views.redisterPage, name="register"),
@@ -19,5 +20,14 @@ urlpatterns = [
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
 
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_sent/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
 ]
+
+# 1.sumbi format
+# 2.Email sent success message
+# 3.Link to reset password
+# 4.password successfully changed message
